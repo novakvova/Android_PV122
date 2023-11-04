39c8b977a13c7e9b185293ddbec7e08e193e7571
novakvova <novakvova@gmail>
2023-11-04 17:49:47 +0200

    Working app is show Image android

--- AndroidStore/app/src/main/java/com/example/mystore/MainActivity.java
+++ AndroidStore/app/src/main/java/com/example/mystore/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.mystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.mystore.contants.Urls;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView myImage = findViewById(R.id.myImage);
+        String url = Urls.BASE+"/images/123.jpg";
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(myImage);
+
     }
 }
