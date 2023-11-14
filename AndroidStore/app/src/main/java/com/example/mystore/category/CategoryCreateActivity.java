1408091bdb6a7d1780821ea92ff0fd375d2eb664
novakvova <novakvova@gmail>
2023-11-14 19:25:24 +0200

    Begin working app

---
+++ AndroidStore/app/src/main/java/com/example/mystore/category/CategoryCreateActivity.java
@@ -0,0 +1,17 @@
+package com.example.mystore.category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mystore.BaseActivity;
+import com.example.mystore.R;
+
+public class CategoryCreateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
