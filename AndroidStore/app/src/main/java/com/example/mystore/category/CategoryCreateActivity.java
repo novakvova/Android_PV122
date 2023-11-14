a6e07443b4cccb41dddb1f28a53b68c31e72ee16
novakvova <novakvova@gmail>
2023-11-14 20:41:23 +0200

    Add working crate category

--- AndroidStore/app/src/main/java/com/example/mystore/category/CategoryCreateActivity.java
+++ AndroidStore/app/src/main/java/com/example/mystore/category/CategoryCreateActivity.java
@@ -2,16 +2,56 @@ package com.example.mystore.category;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.mystore.BaseActivity;
 import com.example.mystore.R;
+import com.example.mystore.application.HomeApplication;
 
 public class CategoryCreateActivity extends BaseActivity {
 
+    private static final int PICK_IMAGE_SELECTED = 1;
+    ImageView ivPreview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_create);
+
+        ivPreview = findViewById(R.id.ivPreview);
+        String url = "https://icons.veryicon.com/png/o/miscellaneous/general-icon-library/preview-65.png";
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(300))
+                .into(ivPreview);
     }
+
+    public void onSelectImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_SELECTED);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_SELECTED && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Get the URI of the selected image
+            Uri uri = data.getData();
+
+            Glide
+                    .with(HomeApplication.getAppContext())
+                    .load(uri)
+                    .apply(new RequestOptions().override(300))
+                    .into(ivPreview);
+
+        }
+    }
+
 }
