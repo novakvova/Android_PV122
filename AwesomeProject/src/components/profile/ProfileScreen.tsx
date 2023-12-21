8e58e7bd2647e06ca0f3226dae21a4df401eef12
novakvova <novakvova@gmail.com>
2023-12-21 17:55:22 +0200

    Begin working porfile

--- AwesomeProject/src/components/profile/ProfileScreen.tsx
+++ AwesomeProject/src/components/profile/ProfileScreen.tsx
@@ -3,6 +3,11 @@ import * as React from 'react'
 import {Text, View} from 'react-native'
 import {useSelector} from "react-redux";
 import {IAuthReducer} from "../account/types";
+import PropTypes from 'prop-types'
+
+import contactData from '../mocks/contact.json';
+
+import Profile from './Profile'
 
 
 const ProfileScreen = () => {
@@ -10,9 +15,7 @@ const ProfileScreen = () => {
 
     console.log("User auth", user);
     return (
-        <View>
-            <Text>{user?.name}</Text>
-        </View>
+        <Profile {...contactData} />
     )
 }
 
